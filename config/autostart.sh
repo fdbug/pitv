9aa90144277b7ce1dc67198e43f0d42fa1fe6d7d
Youjie Zhou <jmpopush@gmail.com>
2014-11-08 13:21:33 -0500

    improve aria2 - add an autostart script - use config file - bug fix

---
+++ config/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start aria2 server.
+if [[ -e /storage/local/start-aria2.sh ]]; then
+    sh /storage/local/start-aria2.sh
+fi
--- cross/start-aria2.sh
+++ cross/start-aria2.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-CWD=$(pwd)
-${CWD}/aria2c --enable-rpc --rpc-listen-all 2>&1 > /dev/null &
+#!/bin/bash
+CWD=$(dirname $(readlink -f "$0"))
+if [[ ! -z ${CWD}/aria2.session ]]; then
+    touch ${CWD}/aria2.session
+fi
+${CWD}/aria2c --conf-path=${CWD}/aria2.conf -D
